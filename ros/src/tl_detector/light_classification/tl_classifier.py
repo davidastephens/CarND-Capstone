ffdff393b94219ec09a0814a0356ad9665810d58
David Stephens <david@davidstephens.io>
2018-04-07 17:05:06 -0600

    Clean up tl_classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -8,27 +8,23 @@ import sys
 import tensorflow as tf
 import numpy as np
 
-
 # Traffic Light Detection Based on Tensorflow demo shown here:
 # https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 DIR_PATH = os.path.join(FILE_PATH, "images")
 OUTPUT_DIR_PATH = os.path.join(FILE_PATH, "images_out")
-OBJECT_DETECTION_PATH = os.path.join(FILE_PATH)
-sys.path.insert(0, OBJECT_DETECTION_PATH)
 
-from object_detection.utils import label_map_util
 from utils import visualization_utils as vis_util
 
 MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
 PATH_TO_CKPT = os.path.join(FILE_PATH, MODEL_NAME, 'frozen_inference_graph.pb')
-PATH_TO_LABELS = os.path.join(FILE_PATH, 'object_detection', 'data', 'mscoco_label_map.pbtxt')
 NUM_CLASSES = 90
+
 
 class TLClassifier(object):
     def __init__(self):
 
-        #Load tensorflow graph into memory
+        # Load tensorflow graph into memory
         self.graph = tf.Graph()
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
@@ -40,22 +36,8 @@ class TLClassifier(object):
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
 
-        label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
-                                                                    use_display_name=True)
-        category_index = label_map_util.create_category_index(categories)
-
-        self.label_map = label_map
-        self.categories = categories
-        self.category_index = category_index
-
-    @staticmethod
-    def load_image_into_numpy_array(image):
-        (im_width, im_height) = image.size
-        return np.array(image.getdata()).reshape(
-            (im_height, im_width, 3)).astype(np.uint8)
-
     def run_inference_for_single_image(self, image):
+
         with self.graph.as_default():
             with tf.Session() as sess:
                 # Get handles to input and output tensors
@@ -70,21 +52,7 @@ class TLClassifier(object):
                     if tensor_name in all_tensor_names:
                         tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
                             tensor_name)
-                if 'detection_masks' in tensor_dict:
-                    # The following processing is only for single image
-                    detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-                    detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-                    # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-                    real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
-                    detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
-                    detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
-                    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-                        detection_masks, detection_boxes, image.shape[0], image.shape[1])
-                    detection_masks_reframed = tf.cast(
-                        tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-                    # Follow the convention by adding back the batch dimension
-                    tensor_dict['detection_masks'] = tf.expand_dims(
-                        detection_masks_reframed, 0)
+
                 image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
                 # Run inference
@@ -97,8 +65,7 @@ class TLClassifier(object):
                     'detection_classes'][0].astype(np.uint8)
                 output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
                 output_dict['detection_scores'] = output_dict['detection_scores'][0]
-                if 'detection_masks' in output_dict:
-                    output_dict['detection_masks'] = output_dict['detection_masks'][0]
+
         return output_dict
 
     def get_classification(self, image):
@@ -111,24 +78,24 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        # TODO implement light color prediction
         return TrafficLight.UNKNOWN
 
 
 if __name__ == '__main__':
     image_files = glob.glob(os.path.join(DIR_PATH, '*.*'))
+
     classifier = TLClassifier()
     for image_file in image_files:
         img = cv2.imread(image_file)
         _, filename = os.path.split(image_file)
         output_dict = classifier.run_inference_for_single_image(img)
         vis_util.visualize_boxes_and_labels_on_image_array(
-           img,
-           output_dict['detection_boxes'],
-           output_dict['detection_classes'],
-           output_dict['detection_scores'],
-           classifier.category_index,
-           instance_masks=output_dict.get('detection_masks'),
-           use_normalized_coordinates=True,
-           line_thickness=8)
+            img,
+            output_dict['detection_boxes'],
+            output_dict['detection_classes'],
+            output_dict['detection_scores'],
+            use_normalized_coordinates=True,
+            agnostic_mode=True,
+            line_thickness=8)
         cv2.imwrite(os.path.join(OUTPUT_DIR_PATH, filename), img)
