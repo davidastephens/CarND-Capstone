1745ba809bfc9784e00552d95ac0b030c81ea909
David Stephens <david@davidstephens.io>
2018-04-15 14:08:00 -0600

    Update nvidia-docker so rviz can be used

--- nvidia_docker_run.sh
+++ nvidia_docker_run.sh
@@ -5,11 +5,16 @@ set -e
 UDACITY_SOURCE=${UDACITY_SOURCE:-`pwd`}
 CONTAINER_NAME="capstone"
 UDACITY_IMAGE=${UDACITY_IMAGE:-tantony/carnd-capstone-cuda}
- 
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
 if [ "$(docker ps | grep ${CONTAINER_NAME})" ]; then
   echo "Attaching to running container..."
   nvidia-docker exec -e DISPLAY=$DISPLAY -it ${CONTAINER_NAME} bash $@
 else
-  nvidia-docker run -e DISPLAY=$DISPLAY --name ${CONTAINER_NAME} --rm -it -p 4567:4567 -v "${UDACITY_SOURCE}:/capstone" ${UDACITY_IMAGE} $@ 
+  nvidia-docker run -e DISPLAY=$DISPLAY -e XAUTHORITY=${XAUTH} --name ${CONTAINER_NAME} --rm -it -p 4567:4567 -v "${UDACITY_SOURCE}:/capstone" -v ${XSOCK}:${XSOCK} ${UDACITY_IMAGE} $@ 
 
 fi
