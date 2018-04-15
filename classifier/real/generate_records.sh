db3ae03941a99773ea143e4e1708c198b8649f49
David Stephens <david@davidstephens.io>
2018-04-15 14:35:47 -0600

    Update training data generation scripts

--- classifier/real/generate_records.sh
+++ classifier/real/generate_records.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=$PYTHONPATH:.:..
-python generate_tfrecord.py --csv_input=data/train.csv  --output_path=data/train.record
-python generate_tfrecord.py --csv_input=data/test.csv  --output_path=data/test.record
+python3 generate_tfrecord.py --csv_input=data/train.csv  --output_path=data/train.record
+python3 generate_tfrecord.py --csv_input=data/test.csv  --output_path=data/test.record
